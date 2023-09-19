3797718cbf772e0572a5cc789a71d57b7afd5bc8
yewinnaingedem <yewinnaing11605@gmail.com>
2023-09-19 05:19:46 -0700

    modified

--- resources/js/Component/step1.vue
+++ resources/js/Component/step1.vue
@@ -1,81 +1,213 @@
 <template>
-    <div class="main-controller">
-        <div class="main-color">
-            <div class="react-project">
-                <div class="content-1">
-                    <aside>
-                        <div>
-                            <h1 class="logo">Shift </h1>
-                        </div>
-                        <ol class="content-body">
-                            <li>
-                                <div class="flex">
-                                    <div class="mr-3">1</div>
-                                    <div>Basic Details </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="flex">
-                                    <div class="mr-3">2</div>
-                                    <div>Additonal Fecture </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="flex">
-                                    <div class="mr-3">3</div>
-                                    <div>Vehicle History </div>
-                                </div>
-                            </li>
-                        </ol>
-                    </aside>
+    <div class="row">
+        <div  class="col-md-4  main-color w-full vh-100">
+            <div class="header   ">
+                <div class="d-flex justify-content-center w-full  align-items-start ">
+                    <h1 class="mt-3">Shift MM </h1>
                 </div>
-                <div class="content-2">
-                    <div class="flex-between">
-                        <div class="flex">
-                            <div class="mr-3">Suzuki</div>
-                            <div class="mr-3 font-bold">2574</div>
-                            <div>7777</div>
+            </div>
+            <div class="d-flex justify-content-center flex-column  align-items-start pl-25 h-75">
+                <div class="d-flex w-full justify-content-center  fw-semibold  font-weight-normal">
+                    <span class="mr-3">1</span>
+                    <div>
+                        <p>Basic Details </p>
+                    </div>
+                </div>
+                <div class="d-flex w-full justify-content-center fw-semibold  font-weight-normal">
+                    <span class="mr-3">2</span>
+                    <div>
+                        <p>Additional Fecuture </p>
+                    </div>
+                </div>
+                <div class="d-flex w-full justify-content-center fw-semibold  font-weight-normal">
+                    <span class="mr-3">3</span>
+                    <div>
+                        <p>Vehicle History </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div  class="col-md-8">
+            <div class="position-sticky   z-100 bg-white w-100">
+                <div class="header  d-flex justify-content-between border-bottom p-3">
+                    <div class="d-flex justify-content-center align-items-center font-normal">
+                        <div class="mr-2">
+                            2016
                         </div>
-                        <div class="">
-                            <a href="">Exit </a>
+                        <div class="fw-bold mr-2 capitalize">
+                            Suzuki 
+                        </div>
+                        <div class="fw-normal capitalize">
+                            ciaz 
                         </div>
                     </div>
-                    <div class="main-content">
-                        <div class="content">
-                            <div>
-                                <h1>Basic Details</h1>
-                                <div class="mt-20px">
-                                    <div class="group1">
-                                        <label for="" class="d-flex flex-row">
-                                            <label for="">Zip</label>
-                                            <input type="text" name="" id="" placeholder="Zip">
-                                            <p>place fill your zip code </p>
-                                        </label>
-                                    </div>
-                                    <div class="group1">
-                                        <label for="" class="d-flex flex-row">
-                                            <label for="">Millage</label>
-                                            <input type="text" name="" id="" placeholder="Enter Millage">
-                                            <p>ivvalid your code  </p>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h1>Exterior Color </h1>
-                                    <div class="mt-20px">
-                                        <label for="" class="w-full">
-                                            <input type="text" placeholder="Enter Exterior Color " class="d-block">
-                                        </label>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h1>Body Style</h1>
-                                    <label for="" class="select-group">
-                                        <select name="body-Style" id="">
-                                            <!-- <option value="4_door_suv" selected>4 Door SUV</option> -->
-                                        </select>
-                                    </label>
-                                </div>
+                    <div class="color-primary">
+                        <a href="/admin/add-cars" class="text-decoration-none">Exit</a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="main w-75 m-auto pt-20">
+                <h3 class="fw-bolder">Basic Details </h3>
+                <div class="row">
+                    <div class="col-md-6 ">
+                        <div class="mb-3">
+                            <div class="d-flex flex-column justify-content-center align-items-start ">
+                                <label for="zip" class="form-label">Zip</label>
+                                <input type="text" name="" id="zip" class="w-100 form-control mb-1" placeholder="Enter Zip">
+                                <p class="fs-8 text-danger">invaild zip code</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 col-md-6">
+                            <div class="d-flex flex-column justify-content-center align-items-start ">
+                                <label for="millage" class="form-label">Millage</label>
+                                <input type="text" name="" id="millage" class="w-100 form-control mb-1" placeholder="Enter Millage">
+                                <p class="fs-8 text-danger">invaild Millage</p>
+                            </div>
+                        </div>
+                </div>
+                <div class="mb-3">
+                    <div class="d-flex justify-content-between align-items-center mb-2 ">
+                        <h5 class="fw-bold">Trim</h5>
+                        <i class="fa-solid fa-question"></i>
+                    </div>
+                    <select class="form-select" aria-label="">
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <h5 class="fw-bold cap-5">Exterior color</h5>
+                    <div class=" row " >
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="black">
+                            <label class="form-check-label" for="black">
+                                Black 
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="white">
+                            <label class="form-check-label" for="white">
+                                White 
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="pearl_white">
+                            <label class="form-check-label" for="pearl_white">
+                                Pearl White 
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="gray">
+                            <label class="form-check-label" for="gray">
+                                Gray 
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="red">
+                            <label class="form-check-label" for="red">
+                                Red  
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="pearl_gold">
+                            <label class="form-check-label" for="pearl_gold">
+                                Pearl Gold  
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="vory">
+                            <label class="form-check-label" for="vory">
+                                Ivory White 
+                            </label>
+                        </div>
+                        <div class="mb-3 col-md-3">
+                            <input class="form-check-input mr-5" type="radio" name="color" id="blue">
+                            <label class="form-check-label" for="blue">
+                                Blue 
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <h5 class="fw-bold cap-5"> Body Style</h5>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-md-3 mb-3">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="4_doors">
+                                <label class="form-check-label" for="4_door">
+                                    4 Door SUV
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="sedan">
+                                <label class="form-check-label" for="sedan">
+                                    Sedon 
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="mvp">
+                                <label class="form-check-label" for="mvp">
+                                    MVP
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="el_hy">
+                                <label class="form-check-label" for="el_hy">
+                                    Electric / Hybird 
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="hatchback">
+                                <label class="form-check-label" for="hatchback">
+                                    Hatchback 
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="crossover">
+                                <label class="form-check-label" for="crossover">
+                                    Crossover 
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="coupe">
+                                <label class="form-check-label" for="coupe">
+                                    Coupe
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="convertible">
+                                <label class="form-check-label" for="convertible">
+                                    Convertible
+                                </label>
+                            </div>
+
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="minivan">
+                                <label class="form-check-label" for="minivan">
+                                    Minivan
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="pickup">
+                                <label class="form-check-label" for="pickup">
+                                    Pickup Truck 
+                                </label>
+                            </div>
+                            <div class="col-md-3 mb-3 ">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="wagon">
+                                <label class="form-check-label" for="wagon">
+                                    Wagon
+                                </label>
+                            </div>
+
+                            <div class="col-md-3 mb-3">
+                                <input class="form-check-input mr-5" type="radio" name="body_styel" id="luxury">
+                                <label class="form-check-label" for="luxury">
+                                    Luxury Cars
+                                </label>
                             </div>
                         </div>
                     </div>
@@ -85,187 +217,46 @@
     </div>
 </template>
 
-<style lang="css">
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200;0,6..12,300;0,6..12,400;0,6..12,500;1,6..12,200;1,6..12,300;1,6..12,400;1,6..12,500&family=Poppins:ital,wght@0,300;0,500;1,200;1,300&family=Roboto:ital,wght@0,100;0,300;1,100&display=swap');
-* , *::after , *::before    {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-.mt-20px {
-    display: flex;
-    margin-top: 30px;
-
-}
-a {
-    text-decoration: none;
-}
-.group1 {
-    width: 100%;
-    padding-right: 15px;
-    margin-bottom: 15px;
-}
-.select-group {
-    border: 1px solid gray;
-    position: relative;
-
-}
-.select-group select {
-    padding: 10px 20px;
-    appearance: none;   
-    text-overflow: ellipsis;
-    outline: none;
-    color: #34435c;
-    background-color: transparent;
-    line-height: 19px;
-    height: 44px;
-    width: 100%;
-    padding-right: 30px;
-    padding-left: 12px;
-    border: none;
-}
-.group1 .d-flex p {
-    color : red ;
-    font-size: 12px;
-    margin-top: 5px;
-}
-.d-block {
-    display: block;
-    width: 100%;
-}
-.group-radio {
-    margin-right: 14px;
-    margin-bottom: 14px;
-    color: #fff;
-    background-color: #34435c;
-    border-color: #34435c;
-    font-weight: 500;
-    border-radius: 4px;
-    cursor: pointer;
-    text-decoration: none;
-    white-space: nowrap;
-    height: 48px;
-    min-height: 48px;
-    font-size: 16px;
-    outline: none;
-    padding: 14px 20px;
-}
-.content {
-    margin: 64px auto;
-    max-width: 560px;
-}
-.d-flex {
-    display: flex;
-}
-.flex-row {
-    flex-direction: column;
-    
-}
-.radio-input {
-    display: none;
-    appearance: none;
-}
-label {
-    font-size: 16px;
-    font-weight: 600;
-    margin-bottom: 2px;
-}
-input {
-    padding: 10px 20px;
-    border-radius: 4px;
-    outline: none;
-    border: 1px solid gray;
-}
-.main-content {
-    padding:  0 18px;
-    position: relative;
-    overflow-y: auto;
-    background-color: #fff;
-
-}
-.main-controller {
-    color:  "#18222e";    
-}
-.content-2 {
-    width: 100%;
-}
-.font-bold {
-    font-weight: 500;
-}
-.main-color {
-    background-color: "#f7f7f7";
-    min-height: 100vh;
-}
-.flex-between {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    overflow: hidden;
-    padding: 30px 15px;
-    border-bottom: 2px solid #f7f7f7;
-}
-.react-project {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    overflow: hidden;
-    min-height: 100vh;
-    flex-direction: row;
-}
-.content-1 {
-    display: flex;
-    width: 351px;
-    background-color: #f7f7f7;
-}
-aside {
-    height: 100%;
-    flex : 0 1 351px ;
-}
-.flex {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.mr-3{
-    margin-right: 5px;
-}
-.logo {
-    padding: 0;
-    border: 0 none;
-    cursor: pointer;
-    line-height: inherit;
-    width: 72px;
-    height: 22px;
-    margin: 30px 24px;
-    color: #06CBA3;
-    font-weight: 700;
-}
-ol li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
-    font-weight: 500;
-    font-size: 16px;    
-    letter-spacing: 0;
-    font-family: "Roboto" , sans-serif;
-    line-height: 24px;
-    width: 50%;
-    margin: 30px auto;
-}
-.content-body{
-    margin: 60px auto;
-}
-.w-full {
-    width: 100%;
-}
+<style>
+    .mr-3
+    {
+        margin-right: 15px;
+    }
+    .mr-5 {
+        margin-right: 5px;
+    }
+    .fs-8 {
+        font-size: 12px;
+    }
+    .pl-25 {
+        padding-left: 25px;
+    }
+    .cap-5 {
+        opacity: 0.8 ;
+    }
+    .position-sticky  {
+        top: 0px ;
+        left: 0;
+    }
+    .pt-20 {
+        padding-top: 20px;
+    }
+    .mr-2 {
+        margin-right: 5px;
+    }
+    .capitalize {
+        text-transform: capitalize;
+    }
+    .main-color {
+        background-color: #f7f7f7;
+    }
+    .z-100 {
+        z-index: 100;
+    }
 </style>
 
 <script>
     import $ from 'jquery'
 
     
-</script>
+</script> 
--- resources/views/Admin/Cars/stepProgess.blade.php
+++ resources/views/Admin/Cars/stepProgess.blade.php
@@ -6,11 +6,13 @@
     <title>Step stepProgess</title>
     <link rel="stylesheet" href="{{asset('storage/style/main.css')}}">
     @vite('resources/js/app.js')
+    <link href="{{asset('storage/admin/css/styles.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div  id="app">
         <step1/>
     </div>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js" integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
