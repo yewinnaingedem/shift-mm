207d994df719479e703c8a9aa8e28938d5edb0f0
yewinnaingedem <yewinnaing11605@gmail.com>
2023-10-08 05:43:47 -0700

    modified

--- resources/views/Admin/Cars/info.blade.php
+++ resources/views/Admin/Cars/info.blade.php
@@ -5,6 +5,11 @@
 @section('style')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <style> 
+        .mr-3 {
+            margin-right : 5px ;
+        }
+    </style>
 @endsection 
 
 @section('navbar') 
@@ -27,7 +32,7 @@
         </thead>
         <tbody>
             @foreach($car_infos as $info )
-                <tr>
+                <tr class="text-capitalize">
                     <td>{{$info->name}}</td>
                     <td>{{$info->year}}</td>
                     <td>{{$info->modal}}</td>
@@ -39,7 +44,6 @@
                         <button  class="btn show-data btn-primary" id='show' data-id="{{$info->main_id}}" >
                             View
                         </button>
-                        
                     </td>
                 </tr>
             @endforeach
@@ -63,15 +67,20 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modal_label"></h1>
+        <h1 class="modal-title fs-5" id="modal_label">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class='mr-3'>2006 </div>
+                <div class='mr-3 fw-bolder'>/ Toyato /</div>
+                <div>Wish</div>
+            </div>
+        </h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cencle</button>
+        <button type="button" class="btn btn-primary">Save</button>
       </div>
     </div>
   </div>
@@ -119,6 +128,25 @@
                         "_token" : "{{csrf_token()}}"
                     },
                     success : (response) => {
+                        let data = response[0] ;
+                        let modal_header = `
+                        <div class="row">
+                            <div class="col-md-4 mb-2">
+                                <label for="lincese" class="form-label">License</label>
+                                <input type="text" name="lincese" id="lincese" value="${data.license}" class="form-control">
+                            </div>
+                            <div class="col-md-4 mb-2">
+                                <label for="lincese" class="form-label">Kilo</label>
+                                <input type="text" name="kilo" id="lincese" value="${data.millage}" class="form-control">
+                            </div>
+                            <div class="col-md-4 mb-2">
+                                <label for="lincese" class="form-label">Grade</label>
+                                <input type="text" name="kilo" id="lincese" value="${data.trim}" class="form-control">
+                            </div>
+                        </div>
+                        `;
+                        console.log(response);
+                        $('.modal-body').html(modal_header);
                         $('#view').modal('show');
                     },
                     error : (errorData) => {
@@ -126,7 +154,6 @@
                     }
                 });
                 
-                // $('#view').modal('show');
             }));
         });
     </script>
