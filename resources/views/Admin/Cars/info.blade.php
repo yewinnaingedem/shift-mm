6a7be8a23ca641d6bca663993ed3bc8484353eb8
yewinnaingedem <yewinnaing11605@gmail.com>
2023-10-08 07:21:31 -0700

    modified

--- resources/views/Admin/Cars/info.blade.php
+++ resources/views/Admin/Cars/info.blade.php
@@ -9,6 +9,9 @@
         .mr-3 {
             margin-right : 5px ;
         }
+        .mb-1.5 {
+            margin-bottom : 1.5px ;
+        }
     </style>
 @endsection 
 
@@ -134,32 +137,133 @@
                         `;
                         let modal_header = `
                         <div class="row">
-                            <div class="col-md-4 mb-2">
+                            <div class="col-md-4 mb-3">
                                 <label for="lincese" class="form-label">License</label>
                                 <input type="text" name="lincese" id="lincese" value="${data.license}" class="form-control">
                             </div>
-                            <div class="col-md-4 mb-2">
-                                <label for="lincese" class="form-label">Kilo</label>
-                                <input type="text" name="kilo" id="lincese" value="${data.millage}" class="form-control">
+                            <div class="col-md-4 mb-3">
+                                <label for="kilo" class="form-label">Kilo</label>
+                                <input type="text" name="kilo" id="kilo" value="${data.millage}" class="form-control">
                             </div>
-                            <div class="col-md-4 mb-2">
-                                <label for="lincese" class="form-label">Grade</label>
-                                <input type="text" name="kilo" id="lincese" value="${data.trim}" class="form-control">
+                            <div class="col-md-4 mb-3">
+                                <label for="grade" class="form-label">Grade</label>
+                                <input type="text" name="grade" id="grade" value="${data.trim == 1 ? "Doesn't have Grade" : data.trim }" class="form-control">
                             </div>
-                            <div class="col-md-4 mb-5">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="221" id="flexCheckChecked" checked>
-                                    <label class="form-check-label" for="flexCheckChecked">
-                                        Blind Sport 
-                                    </label>
+                            <div class="col-md-4 mb-3">
+                                <label for="divertrim" class="form-label">Divertrim</label>
+                                <input type="text" name="divertrim" id="divertrim" value="${data.divertrim}" class="form-control">
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <label for="camera" class="form-label">Camera</label>
+                                <input type="text" name="camera" id="camera" value="${data.camera}" class="form-control">
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <label for="seat_leather" class="form-label">Seat</label>
+                                <input type="text" name="seat_leather" id="seat_leather" value="${data.seat_leather}" class="form-control">
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <label for="transmission" class="form-label">Transmission</label>
+                                <input type="text" name="transmission" id="transmission" value="${data.transmission}" class="form-control">
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col-md-6">
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="abs" ${Boolean(data.abs) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="abs">
+                                            A B S 
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="auto_em_b" ${Boolean(data.auto_em_b) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="auto_em_b">
+                                            Auto Emergency Breake
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="auto_em_b" ${Boolean(data.auto_em_b) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="auto_em_b">
+                                            Auto Emergency Breake
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="auto_hold" ${Boolean(data.auto_hold) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="auto_hold">
+                                            Auto Hold
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="blind_sprot" ${Boolean(data.blind_sprot) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="blind_sprot">
+                                            Blind Sport 
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="kick_sensor" ${Boolean(data.kick_sensor) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="kick_sensor">
+                                            Kick Sensor
+                                        </label>
+                                    </div>
+                                    
+                                </div>
+                                <div class="col-md-6" > 
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="tire_pressure" ${Boolean(data.tire_pressure) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="tire_pressure">
+                                            Tire Pressure
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="truck_motor" ${Boolean(data.truck_motor) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="truck_motor">
+                                            Truck Motor
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="lane_keep_assit" ${Boolean(data.lane_keep_assit) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="lane_keep_assit">
+                                            Lane Keep Assit 
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="rain_sensor" ${Boolean(data.rain_sensor) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="rain_sensor">
+                                            Rain Sensor
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="rounded_ac" ${Boolean(data.rounded_ac) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="rounded_ac">
+                                            Rounded AirCon
+                                        </label>
+                                    </div>
+                                    <div class="mb-1.5">
+                                        <input class="form-check-input" type="checkbox"  id="streeing_volume" ${Boolean(data.streeing_volume) ? 'checked' : ' '}>
+                                        <label class="form-check-label" for="streeing_volume">
+                                            Streeing Volume
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-4 mb-2">
+                                    <label for="key" class="form-label">Key</label>
+                                    <input type="text" name="key" id="key" value="${data.key}" class="form-control">
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <label for="engine " class="form-label">Engine</label>
+                                    <input type="text" name="engine" id="engine" value="${data.engine}" class="form-control">
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <label for="exterior_color" class="form-label">exterior_color</label>
+                                    <input type="text" name="exterior_color" id="exterior_color" value="${data.exterior_color}" class="form-control">
                                 </div>
                             </div>
                         </div>
                         `;
-                        // console.log(response['car_data']);
                         $('#modal_label').html(modal_title);
                         $('.modal-body').html(modal_header);
                         $('#view').modal('show');
+                        console.log(data);
                     },
                     error : (errorData) => {
                         console.log(errorData);
