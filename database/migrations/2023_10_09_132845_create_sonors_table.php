63622bd4f688ee62a83b9d79cb1c70783888ba44
yewinnaingedem <yewinnaing11605@gmail.com>
2023-10-09 07:06:32 -0700

    modified

--- database/migrations/2023_10_09_132845_create_sonors_table.php
+++ database/migrations/2023_10_09_132845_create_sonors_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('sonors', function (Blueprint $table) {
             $table->id();
+            $table->string('sonor');
             $table->timestamps();
         });
     }
